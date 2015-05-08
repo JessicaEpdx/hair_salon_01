53c7bac62f928d53f37ca74869cd46ad6a54a56d
Jessica Engel <Jessicaepdx@Gmail.com>
2015-05-08 15:18:09 -0700

    final product

--- spec/hair_salon_integration_spec.rb
+++ spec/hair_salon_integration_spec.rb
@@ -4,7 +4,10 @@ require('spec_helper')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('add client form path', {:type => :feature}) do
+
+
+
+describe('add client/stylist form path', {:type => :feature}) do
   it('allows user to add client or stylist, sends user to list of clients or stylists') do
     visit('/')
     fill_in('client_name', :with => 'Deborah Gibson')
@@ -14,18 +17,5 @@ describe('add client form path', {:type => :feature}) do
     fill_in('stylist_name', :with => 'Harry Potter')
     click_button('Add Stylist')
     expect(page).to have_content('Harry Potter')
-  end
-end
-
-describe('individual client and stylist page path') do
-  it('send user to individual pages when name link is clicked') do
-    client = Client.new({:name => "Bill", :id => nil, :stylist_id => nil})
-    client.save()
-    stylist = Stylist.new({:name => "Bob", :id => nil})
-    stylist.save()
-    visit("/clients/#{client.id()}")
-    expect(page).to have_content("Bill")
-    visit("/stylists/#{stylist.id()}")
-    expect(page).to have_content("Bob")
   end
 end
