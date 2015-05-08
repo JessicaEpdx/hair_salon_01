c04ed86dc3b133bbb9c3cd8ded7989b1efd86305
Jessica Engel <Jessicaepdx@Gmail.com>
2015-05-08 09:59:40 -0700

    add client to database, save, see all, update name, delete client

--- lib/client.rb
+++ lib/client.rb
@@ -1,4 +1,42 @@
 class Client
+  attr_reader(:name, :id)
+
+    def initialize (attributes)
+      @name = attributes.fetch(:name)
+      @id = attributes.fetch(:id).to_i
+    end
+
+    def save
+      result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING ID;")
+      @id = result.first.fetch("id")
+    end
+
+    def self.all
+      all_clients = [ ]
+      clients = DB.exec("SELECT * FROM clients;")
+      clients.each() do |client|
+        name = client.fetch("name")
+        id = client.fetch("id").to_i
+        all_clients.push(Client.new({:name => name, :id => id}))
+      end
+      all_clients
+    end
+
+    def == (other_client)
+      self.name() == other_client.name()
+    end
+
+    def delete
+      DB.exec("DELETE FROM clients WHERE id = #{self.id()};")
+    end
+
+    def update (attributes)
+      @name = attributes.fetch(:name, @name)
+      @stylist_id = attributes.fetch(:stylist_id, @stylist_id)
+      DB.exec("UPDATE clients SET name = '#{@name}' WHERE id = #{self.id()};")
+      # DB.exec("UPDATE clients SET stylist_id = #{stylist_id} WHERE id = #{self.id()};")
+    end
+
 
 
 
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,1 +1,55 @@
 require('spec_helper')
+
+describe(Client) do
+  describe('#save') do
+    it("saves client to database") do
+      test_client = Client.new({:name => "Sharon", :id => nil})
+      test_client.save()
+      expect(Client.all()).to(eq([test_client]))
+    end
+  end
+
+    describe('#name') do
+      it("returns name of client") do
+        test_client = Client.new({:name => "Clinton", :id => nil})
+        test_client.save()
+        expect(test_client.name()).to(eq("Clinton"))
+      end
+    end
+
+    describe('.all') do
+      it("returns all clients in database") do
+        test_client = Client.new({:name => "Clinton", :id => nil})
+        test_client.save()
+        test_client2 = Client.new({:name => "Sharon", :id => nil})
+        test_client2.save()
+        expect(Client.all()).to(eq([test_client, test_client2]))
+      end
+    end
+
+    describe('#delete') do
+      it("deletes client from database") do
+        test_client = Client.new({:name => "Clinton", :id => nil})
+        test_client.save()
+        test_client2 = Client.new({:name => "Sharon", :id => nil})
+        test_client2.save()
+        test_client.delete()
+        expect(Client.all()).to(eq([test_client2]))
+      end
+    end
+
+    describe('#update') do
+      it("updates client name") do
+        test_client = Client.new({:name => "Clinton", :id => nil})
+        test_client.save()
+        test_stylist = Stylist.new({:name => "Freddy", :id => nil})
+        test_stylist.save()
+        test_client.update({:name => "Sharon"})
+        expect(test_client.name()).to(eq("Sharon"))
+      end
+    end
+
+
+
+
+end
